5ee09b01c3030e85c3b55d2cc657071844f7b02f
kharondavis <davis.kharon4dev@gmail.com>
2024-03-11 18:11:32 -0400

    Updated function

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-document.getElementById("param-form").addEventListener("submit", function(event) {
-    event.preventDefault();
+function generatePasswords() {
 
-    var formData = new FormData(this);
-
-    fetch(this.action, {
-        method: this.method,
-        body: formData
-    })
 }
