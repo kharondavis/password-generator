11af8a158857c128db828afb19637936dc3abfdb
kharondavis <davis.kharon4dev@gmail.com>
2024-03-18 18:50:19 -0400

    Added script for retrieving user inputs

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function generatePasswords() {
-//get parameters//
-//need math logic for random number/letter generation//
-//need to route result back to element//
+// Step 1: Get user input //
+
+// Retrieve the value entered by the user //
+var passwordAmount = parseInt(document.getElementById("password-amount").value);
+var passwordLength = parseInt(document.getElementById("password-length").value);
+
+// Check if the "include-numbers" checkbox is checked //
+var includeNumbers = document.getElementById("indlude-numbers").checked;
+var includeSpecialChars = document.getElementById("special-char").checked;
+
+// Retrieve the selected option from the "complexity" dropdown
+var passwordComplexity = document.getElementById("complexity").value;
+
+// Step 2: Generate passwords //
+
+
 }
